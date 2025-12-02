e296ca1c5ce7ae6e5a86164dd0f176af5d3fad04
ChanyaponMUT <6811130042>
2025-12-02 18:07:35 +0700

    00

---
+++ LAB03/Test302/Test302.cpp
@@ -0,0 +1,54 @@
+// Test302.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int score;
+    string name;
+    string studentid;
+
+    cout << "Enter Student ID" << endl;
+    cin >> studentid;
+    cin.ignore();
+    cout << "Enter name" << endl;
+    getline(cin,name);
+    cout << "Enter Total score" << endl;
+    cin >> score;
+
+
+
+    cout << "------Student Report------" << endl;
+    cout << "Student ID     :" <<studentid<< endl;
+    cout << "Name     :" << name << endl;
+    cout << "Total score     :"<< score << endl;
+    cout << "Grade     :";
+        if (score >= 90) {
+            cout << "A" << endl;
+        }
+        else if (score >= 80) {
+            cout << "B" << endl;
+        }
+        else if (score >= 70) {
+            cout << "C" << endl;
+        }
+        else {
+            cout << "F" << endl;
+        }
+   cout << "-----------------------------" << endl;
+        return 0;  
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
